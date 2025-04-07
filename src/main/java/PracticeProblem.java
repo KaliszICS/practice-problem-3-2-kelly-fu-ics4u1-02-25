97f3b3dc444a31e1dbc62f1ecf0d586c2157dfda
kelly-fu-ics4u1-02-25 <kelly.fu@student.tdsb.on.ca>
2025-04-07 23:11:32 +0000

    Update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,21 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+	
+ public static int towerOfHanoi(int num){
 
-	}
-
-	public static void q1() {
-		//Write question 1 code here
-	}
-
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
-
+  if (num<3){
+   return -1;
+  }
+  if (num==3){
+   return 7;
+  }
+  else{
+   
+   return 2 * towerOfHanoi(num - 1) +1;
+   
+  }
+ }
+	
 }
+
+
